801e3d3702a100bc644565e4674ba3daa4912048
Martin Matusiak <numerodix@gmail.com>
2011-04-03 11:17:05 +0200

    update

--- app.cs
+++ app.cs
@@ -4,26 +4,26 @@ using Microsoft.Scripting;
 using Microsoft.Scripting.Hosting;
 
 public class App {
-    static void Main(string[] args) {
-		string pyscript = "demo.py";
+	static void Main(string[] args) {
+		string pyscript = "gtk.py";
 		if (args.Length > 0) {
 			pyscript = args[0];
 		}
 
 		ScriptRuntimeSetup scriptRuntimeSetup = new ScriptRuntimeSetup();
 
-        LanguageSetup language = Python.CreateLanguageSetup(null);
-        language.Options["FullFrames"] = true;
-        scriptRuntimeSetup.LanguageSetups.Add(language);
+		LanguageSetup language = Python.CreateLanguageSetup(null);
+		language.Options["FullFrames"] = true;
+		scriptRuntimeSetup.LanguageSetups.Add(language);
 
-        ScriptRuntime runtime = new Microsoft.Scripting.Hosting.ScriptRuntime(scriptRuntimeSetup);
-        ScriptScope scope = runtime.CreateScope();
-        ScriptEngine engine = runtime.GetEngine("python");
-        ScriptSource source = engine.CreateScriptSourceFromFile(pyscript);
-        source.Compile();
+		ScriptRuntime runtime = new Microsoft.Scripting.Hosting.ScriptRuntime(scriptRuntimeSetup);
+		ScriptScope scope = runtime.CreateScope();
+		ScriptEngine engine = runtime.GetEngine("python");
+		ScriptSource source = engine.CreateScriptSourceFromFile(pyscript);
+		source.Compile();
 
-        try {
-            source.Execute(scope);
-        } catch (IronPython.Runtime.Exceptions.SystemExitException e) {}
-    }
+		try {
+			source.Execute(scope);
+		} catch (IronPython.Runtime.Exceptions.SystemExitException e) {}
+	}
 }
