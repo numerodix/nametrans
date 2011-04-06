d1e90fc95caa7239c8746cddf0bdb69504d5cf29
Martin Matusiak <numerodix@gmail.com>
2011-04-06 10:19:17 +0200

    update

--- guinametrans.py
+++ guinametrans.py
@@ -49,6 +49,7 @@ import re
 sys.version = 'ironpython' # is unset when python is hosted on .NET
 import nametrans
 from src import nametransformer
+from src import fs
 from src.nametransformer import NameTransformer
 
 from guisrc.gtkhelper import GtkHelper
@@ -76,6 +77,8 @@ class Application(object):
 
         self.gtkhelper = GtkHelper()
 
+        self.log = LogWindow()
+
         self.init_model()
         self.init_glade()
         self.init_gui()
@@ -84,8 +87,11 @@ class Application(object):
 
     def init_glade(self):
         mypath = os.path.dirname(__file__)
-        gxml = Glade.XML(os.path.join(mypath, self.glade_file), "mainwindow", None)
-        pygladeAutoconnect(gxml, self)
+        def init_widget(name, obj):
+            gxml = Glade.XML(os.path.join(mypath, self.glade_file), name, None)
+            pygladeAutoconnect(gxml, obj)
+        init_widget('mainwindow', self)
+        init_widget('logwindow', self.log)
 
     def init_model(self):
         self.options, _, _ = nametransformer.get_opt_parse(sys.argv)
@@ -116,7 +122,12 @@ class Application(object):
 
         self.button_apply.Clicked += self.do_apply
 
+        # logwindow events
+        self.log.textview_log.Buffer.Changed += self.log.onTextBufferChanged
+        self.log.button_close.Clicked += self.log.onClose
+
     def init_gui(self):
+        # mainwindow
         window_x = 600
         window_y = 500
         windowpadding_x = (self.alignment_main.TopPadding +
@@ -132,6 +143,21 @@ class Application(object):
                                    "text", 1, "background", 3)
         for col in self.fileview.Columns:
             col.MinWidth = (window_x - windowpadding_x) / 2
+
+        # logwindow
+        self.log.logwindow.Title = "Log"
+        self.log.logwindow.SetDefaultSize(500, 300)
+
+        def error_handler(exc):
+            msg = ' '.join(exc.args)
+            s = "%s: %s\n" % (exc.__class__.__name__, msg)
+            self.log.textview_log.Buffer.Text += s
+        fs.error_handler = error_handler
+
+        def f(args):
+            s = "%s\n\n" % args.ExceptionObject
+            self.log.textview_log.Buffer.Text += s
+        GLib.ExceptionManager.UnhandledException += f
 
         # fill in gui from sys.argv input
         self.text_s_from.Text = self.options.s_from
@@ -236,8 +262,21 @@ class Application(object):
             self.set_file_list(self.items)
 
     def do_apply(self, o, args):
+        # XXX logwindow popup test
+#        v = None + 1
         self.program.perform_renames(self.items)
 
+class LogWindow(object):
+    def onTextBufferChanged(self, o, args):
+        # scroll to the bottom
+        it = self.textview_log.Buffer.EndIter
+        mark = self.textview_log.Buffer.CreateMark('default', it, True)
+        self.textview_log.ScrollToMark(mark, 0, 0, 0, 0)
+
+        self.logwindow.ShowAll()
+
+    def onClose(self, o, args):
+        self.logwindow.Hide()
 
 if __name__ == '__main__' or True:
     def f(args):
