9b74b1fcb10c55532aa3dbaeb9c4c5adfefcf1de
Martin Matusiak <numerodix@gmail.com>
2011-04-08 14:05:00 +0200

    set version 0.3x2

--- web/vars.php
+++ web/vars.php
@@ -1,6 +1,6 @@
 <?php 
-	$nametrans_gui_filename = "nametrans-gui-0.3x1.zip";
-	$nametrans_terminal_filename = "nametrans-terminal-0.3x1.zip";
+	$nametrans_gui_filename = "nametrans-gui-0.3x2.zip";
+	$nametrans_terminal_filename = "nametrans-terminal-0.3x2.zip";
 
 	$nametrans_gui_url = "http://downloads.sourceforge.net/nametrans/$nametrans_gui_filename";
 	$nametrans_terminal_url = "http://downloads.sourceforge.net/nametrans/$nametrans_terminal_filename";
