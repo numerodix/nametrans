78bee70c78dff6f3b095b1704a40659de0b4ec99
Martin Matusiak <numerodix@gmail.com>
2011-04-09 00:48:02 +0200

    set version 0.1pre6

--- src/versioninfo.py
+++ src/versioninfo.py
@@ -1,7 +1,7 @@
 # Author: Martin Matusiak <numerodix@gmail.com>
 # Licensed under the GNU Public License, version 3.
 
-release = "0.1pre5"
+release = "0.1pre6"
 authors = ['Martin Matusiak <numerodix@gmail.com>']
 website = "http://nametrans.sf.net"
 desc = "Rename files with regex search/replace semantics."
