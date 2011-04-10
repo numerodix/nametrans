30e0e0d7c3a413f94d8e08c82e66a5457eab3b29
Martin Matusiak <numerodix@gmail.com>
2011-04-10 11:11:18 +0200

    update

---
+++ .gmake2nmake.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import re
+
+def main():
+    s = open('Makefile').read()
+    s = re.sub('gmcs', 'csc', s)
+    s = re.sub(':=', '=', s)
+    s = re.sub('/', '\\\\', s)
+    s = re.sub('\$<', '$**', s)
+    open('Makefile', 'w').write(s)
+
+main()
