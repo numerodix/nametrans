a830aceb7892d18dafd82b4e7b169266c8f93398
Martin Matusiak <numerodix@gmail.com>
2011-04-10 21:45:59 +0200

    update

--- web/vars.php
+++ web/vars.php
@@ -1,10 +1,18 @@
 <?php 
-	$nametrans_gui_filename = "nametrans-gui-0.1pre13.zip";
 	$nametrans_terminal_filename = "nametrans-terminal-0.1pre13.zip";
-
-	$nametrans_gui_url = "http://downloads.sourceforge.net/nametrans/$nametrans_gui_filename";
-	$nametrans_terminal_url = "http://downloads.sourceforge.net/nametrans/$nametrans_terminal_filename";
-
-	$nametrans_gui_filesize = "2.3mb";
 	$nametrans_terminal_filesize = "23kb";
+
+	$nametrans_gui_filename = "nametrans-gui-0.1pre13.zip";
+	$nametrans_gui_filesize = "2.3mb";
+
+	$nametrans_guiwin_filename = "nametrans-gui-0.1pre13.zip";
+	$nametrans_guiwin_filesize = "2.3mb";
+
+	if (eregi("windows", $_SERVER['HTTP_USER_AGENT'])) {
+		$nametrans_gui_filename = $nametrans_guiwin_filename;
+		$nametrans_gui_filesize = $nametrans_guiwin_filesize;
+	}
+
+	$nametrans_terminal_url = "http://downloads.sourceforge.net/nametrans/$nametrans_terminal_filename";
+	$nametrans_gui_url = "http://downloads.sourceforge.net/nametrans/$nametrans_gui_filename";
 ?>
