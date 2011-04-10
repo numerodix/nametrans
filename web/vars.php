9c32d73274109d0c5700d5cb47cafa58710b26a5
Martin Matusiak <numerodix@gmail.com>
2011-04-10 22:06:56 +0200

    set version 0.1pre14

--- web/vars.php
+++ web/vars.php
@@ -1,12 +1,12 @@
 <?php 
-	$nametrans_terminal_filename = "nametrans-terminal-0.1pre13.zip";
+	$nametrans_terminal_filename = "nametrans-terminal-0.1pre14.zip";
 	$nametrans_terminal_filesize = "23kb";
 
-	$nametrans_gui_filename = "nametrans-gui-0.1pre13.zip";
+	$nametrans_gui_filename = "nametrans-gui-0.1pre14.zip";
 	$nametrans_gui_filesize = "2.3mb";
 
-	$nametrans_guiwin_filename = "nametrans-gui-0.1pre13.zip";
-	$nametrans_guiwin_filesize = "2.3mb";
+	$nametrans_guiwin_filename = "nametrans-guiwin-0.1pre14.zip";
+	$nametrans_guiwin_filesize = "10mb";
 
 	if (eregi("windows", $_SERVER['HTTP_USER_AGENT'])) {
 		$nametrans_gui_filename = $nametrans_guiwin_filename;
