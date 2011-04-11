8b6f12fee9b4a78e2af9f95e033facbb061da69f
Martin Matusiak <numerodix@gmail.com>
2011-04-11 15:54:36 +0200

    update

--- gnametrans.py
+++ gnametrans.py
@@ -58,6 +58,8 @@ def pygladeAutoconnect(gxml, target):
     # add all widgets
     for widget in gxml.GetWidgetPrefix(''):
         name = gxml.GetWidgetName(widget)
+        # if widget.fileview exists, this class manages the widget,
+        # thus bind to widget.fileview.fileview
         if not hasattr(target, name):
             setattr(target, name, widget)
         else:
